df42d2458ad4d74430e1f5446efab9fd601a97a9
Nicolas <midia71@outlook.com>
2025-11-23 11:44:53 -0500

    removing everything

--- src/specs/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "../App";
-
-test("renders name title", () => {
-  render(<App />);
-  const name_text = screen.getByText(/Nicolas Arias/i);
-  expect(name_text).toBeInTheDocument();
-});
